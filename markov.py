a8bebd24de87c586412ffa253ed66ac6a9a869a2
vinitha <vinitha.nec@gmail.com>
2023-03-14 21:24:14 -0700

    Run API

--- markov.py
+++ markov.py
@@ -68,3 +68,26 @@ text = open_and_read_file(filenames)
 
 # Get a Markov chain
 chains = make_chains(text)
+
+
+import discord
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('You are a hero!!')
+
+client.run(os.environ["DISCORD_TOKEN"])
